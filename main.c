913b7333bf8d1d5d9e448888e0b59463147e56a9
Milordius <151358199+Milordius@users.noreply.github.com>
2023-11-18 16:51:24 +0500

    main.c

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
-echo "# embersaw" >> README.md
-  git init
-  git add README.md
-  git commit -m "first commit"
-  git branch -M main
-  git remote add origin https://github.com/Milordius/embersaw.git
-  git push -u origin main
+#include <stdio.h>
+int main(int argc, char **argv)
+(
+    printf("Hello world!\n");
+    return 0;
+)
